145a6ddfaab0d0d8b0a705429ce7d657fdbc4499
Danil <Black-Fox31@yandex.ru>
2020-11-08 00:28:15 +0300

    ShopUpdate

---
+++ Prefabs/ButtonWork.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonWork : MonoBehaviour
+{
+	[SerializeField] 
+	public TextMeshProUGUI Title;
+	public TextMeshProUGUI PricePerformance;
+	private GameObject Shop;
+	public int IndexOfType;
+	public Button Button;
+
+    // Start is called before the first frame update
+    public void Start ()
+    {
+    	Button Button = this.gameObject.GetComponent<Button>();
+    	Button.onClick.AddListener(TaskOnClick);
+    	Shop = GameObject.FindGameObjectWithTag("Shop");
+    	//Title.text = "ХУЕТА";
+        
+    }
+
+    void TaskOnClick()
+    {
+		Shop.GetComponent<ShopManager>().BuyGen(IndexOfType);	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Prefabs/CreaterAdv.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreaterAdv : MonoBehaviour
+{
+
+	public GameObject Prefabs; 
+	private GameObject Advertisment;
+	private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	gameManager = FindObjectOfType<GameManager>();
+    	for (int i = 0; i < 3; i++)
+        {
+        	Advertisment = Instantiate(Prefabs, new Vector3(0, 0, 0), Quaternion.identity, this.gameObject.transform );
+            Advertisment.GetComponent<ButtonWork>().IndexOfType = i;
+            Advertisment.GetComponent<ButtonWork>().Title.text = gameManager.generators[i].GeneratorType;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
