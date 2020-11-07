85404d421da47d421020bc3eacb1ddd12fa335a5
Danil <Black-Fox31@yandex.ru>
2020-11-07 23:08:12 +0300

    ShopSTRASHNO NE SMOTRET

---
+++ SibGameJam11/Assets/Scripts/CreaterAdv.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreaterAdv : MonoBehaviour
+{
+
+	public GameObject Prefabs; 
+	private GameObject Advertisment; 
+    // Start is called before the first frame update
+    void Start()
+    {
+    	for (int i = 0; i < 3; i++)
+        {
+        	Advertisment = Instantiate(Prefabs, new Vector3(0, 0, 0), Quaternion.identity, this.gameObject.transform );
+            Advertisment.GetComponent<ButtonWork>().IndexOfType = i;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
