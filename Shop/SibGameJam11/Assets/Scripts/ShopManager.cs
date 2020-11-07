b4f3cfa76d7d0e87a57ea33f8dd1b2fb18341237
Danil <Black-Fox31@yandex.ru>
2020-11-07 13:57:05 +0300

    Изменил проверку покупки

--- Shop/SibGameJam11/Assets/Scripts/ShopManager.cs
+++ Shop/SibGameJam11/Assets/Scripts/ShopManager.cs
@@ -5,47 +5,48 @@ using UnityEngine;
 public class ShopManager : MonoBehaviour
 {
 
-	private GameManager gameManager;
-	[SerializeField] public PricesSelf[] PricesUpSelf; //класс товаров для прокачки своего генератора
+    private GameManager gameManager;
+    [SerializeField] public PricesSelf[] PricesUpSelf; //класс товаров для прокачки своего генератора
 
-	public int[] PricesGen = new int[] { 10, 20, 30}; //ценники для покупки других генераторов
+    public int[] PricesGen = new int[] { 10, 20, 30}; //ценники для покупки других генераторов
 
     void Start()
     {
-    	gameManager = FindObjectOfType<GameManager>();
+        gameManager = FindObjectOfType<GameManager>();
         
     }
     public void BuyUpSelf(int i)
     {
-    	print(Mathf.FloorToInt(gameManager.Electricity));
-    	if (gameManager.Electricity >= PricesUpSelf[i].Cost)
-    	{
-    		if (gameManager.generators[0].ElectricityPerMoment < PricesUpSelf[i].Uprurn)
-    		{
-    			gameManager.Electricity -= PricesUpSelf[i].Cost;
-    			gameManager.generators[0].ElectricityPerMoment = PricesUpSelf[i].Uprurn;
-    			//тут плашка, что продано
-    		}
-    	}
-    	else
-    	{
-    		print("Где деньги Лебовски?");
-    	}
+        print(Mathf.FloorToInt(gameManager.Electricity));
+        if (gameManager.Electricity >= PricesUpSelf[i].Cost)
+        {
+            if (PricesUpSelf[i].Status)
+            {
+                gameManager.Electricity -= PricesUpSelf[i].Cost;
+                gameManager.generators[0].ElectricityPerMoment = PricesUpSelf[i].Uprurn;
+                PricesUpSelf[i].Status = false;
+                //тут плашка, что продано
+            }
+        }
+        else
+        {
+            print("Где деньги Лебовски?");
+        }
 
     }
 
     public void BuyGen(int i)
     {
-    	print(Mathf.FloorToInt(gameManager.Electricity));
-    	if (gameManager.Electricity >= PricesGen[i-1])
-    	{
-    		gameManager.Electricity -= PricesGen[i-1];
-    		gameManager.generators[i].NumberOfGenerators += 1;
-    	}
-    	else
-    	{
-    		print("Где деньги Лебовски?");
-    	}
+        print(Mathf.FloorToInt(gameManager.Electricity));
+        if (gameManager.Electricity >= PricesGen[i-1])
+        {
+            gameManager.Electricity -= PricesGen[i-1];
+            gameManager.generators[i].NumberOfGenerators += 1;
+        }
+        else
+        {
+            print("Где деньги Лебовски?");
+        }
 
     }
 }
@@ -55,4 +56,5 @@ public class PricesSelf
     public string NameOfBuy;
     public int Cost; //цена
     public float Uprurn; //значение увелечения
+    public bool Status = true;
 }
