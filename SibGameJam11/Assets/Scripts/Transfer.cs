1a1ea0fa450753078e77f40951cf9a3d8ceff309
Danil <Black-Fox31@yandex.ru>
2020-11-08 13:53:25 +0300

    Катсцена

---
+++ SibGameJam11/Assets/Scripts/Transfer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transfer : MonoBehaviour
+{
+    public void Start()
+    {
+    	InvokeRepeating("Play", 25.0f, 25.0f);
+        
+    }
+
+    void Play()
+    {
+        SceneManager.LoadScene("HandGenerator");
+        Destroy (this.gameObject);
+    }
+}
